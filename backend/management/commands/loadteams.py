65490c587c905778c0a4687002fc183421658072
Nick <nalmy@email.unc.edu>
2024-10-02 11:06:04 -0400

    Add backend/management/commands/loadteams.py, add dependency Polars

---
+++ backend/management/commands/loadteams.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.utils.crypto import get_random_string
+import polars as pl
+import datetime as dt
+
+
+class Command(BaseCommand):
+    # python help function
+    help = "Load teams into database"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "file",
+            type=str,
+            help="Location of CSV File structured as [Team Number, Start Time, End Time]",
+        )
+
+    def handle(self, *args, **kwargs):
+        file: str = kwargs["file"]
+        if not file.endswith(".csv"):
+            self.stdout.write("Error -- File not in supported format (.csv)")
+            return
+
+        user_table = pl.read_csv(file)
+        if not user_table:
+            self.stdout.write("Error -- File not found.")
+
+        self.stdout.write(user_table)
